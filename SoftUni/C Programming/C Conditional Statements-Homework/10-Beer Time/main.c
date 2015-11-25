8c7e49e0cd5eed72b3c77aca5540381f54bf4881
NikolovV <ventci.nikolov@gmail.com>
2015-11-26 01:56:30 +0200

    C Programming

--- SoftUni/C Programming/C Conditional Statements-Homework/10-Beer Time/main.c
+++ SoftUni/C Programming/C Conditional Statements-Homework/10-Beer Time/main.c
@@ -9,12 +9,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
+
+#define BUFFER 10
+
+size_t remove_new_line(char *str);
 
 int main()
 {
-   
+    char input[BUFFER];
+    fgets(input, BUFFER, stdin);
+    remove_new_line(input);
+
+    char *token = strtok(input, ": ");
+    char *remainder;
+    int hour = (int) strtol(token, &remainder, 10);
+    if (hour < 1 || hour > 12)
+    {
+        printf("Incorrect input.");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(NULL, ": ");
+    int minute = (int) strtol(token, &remainder, 10);
+    if (minute < 0 || minute > 59)
+    {
+        printf("Incorrect input.");
+        exit(EXIT_FAILURE);
+    }
+
+    char *tt = strtok(NULL, ": ");
+    if (strcmp(tt, "AM") != 0 && strcmp(tt, "PM") != 0)
+    {
+        printf("Incorrect input.");
+        exit(EXIT_FAILURE);
+    }
+    else if ((hour >= 1 && tt[0] == 'P') || (hour < 3 && tt[0] == 'A'))
+    {
+        printf("bear time");
+    }
+    else
+    {
+        printf("non-bear time");
+    }
 
     return (EXIT_SUCCESS);
 }
 
+size_t remove_new_line(char *str)
+{
+    int lenght = strlen(str);
+    if (str[lenght - 1] == '\n')
+    {
+        str[lenght - 1] = '\0';
+    }
+    return lenght;
+}
